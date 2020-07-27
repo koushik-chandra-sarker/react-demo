b6588b4c9a63dd929a516b69a95b848bdacafa07
koushik-stack <koushiksk.ks@gmail.com>
2020-07-27 18:35:06 +0600

    Updates

---
+++ precache-manifest.fb771d893e353b9def659ee1365084e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5766471fb5fff27e6ac6fcb369e4ac2",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "7591a5e8fff5b4b4b65a",
+    "url": "/react-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "beb59ef98e86bcb81978",
+    "url": "/react-demo/static/js/2.98674901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo/static/js/2.98674901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7591a5e8fff5b4b4b65a",
+    "url": "/react-demo/static/js/main.c9116ed4.chunk.js"
+  },
+  {
+    "revision": "6bfd721ecd05556a1e11",
+    "url": "/react-demo/static/js/runtime-main.8485398c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
